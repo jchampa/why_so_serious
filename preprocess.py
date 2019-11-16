672119cb1fcf549a28bd88c90ad3cbcbc16c3f2e
andrew128 <andrewwei98@gmail.com>
2019-11-16 13:54:31 -0500

    add code for pre processing image and label data

--- preprocess.py
+++ preprocess.py
@@ -1,68 +1,44 @@
-import pickle
-import numpy as np
-import tensorflow as tf
 import os
+# import imageio
 
-def unpickle(file):
-	"""
-	CIFAR data contains the files data_batch_1, data_batch_2, ...,
-	as well as test_batch. We have combined all train batches into one
-	batch for you. Each of these files is a Python "pickled"
-	object produced with cPickle. The code below will open up a
-	"pickled" object (each file) and return a dictionary.
+def get_data(images_path, labels_path):
+	'''
+	Note that num images per subject varies.
+	Also not every sequence has a label.
+	:param images_path:
+	:param labels_path:
+	:return: data_dictionary(imageID) -> [image as a numpy array, label as float]
+			 imageID created using path
+	'''
+	data_dictionary = {}
 
-	NOTE: DO NOT EDIT
+	# Read in images.
+	for subdir, dirs, files in os.walk(images_path):
+		for file in files:
+			file_path = os.path.join(subdir, file)
+			key = subdir.replace(images_path, '')
+			# image = imageio.imread(file_path)
+			# data_dictionary[key] = [image]
+			data_dictionary[key] = [1]
 
-	:param file: the file to unpickle
-	:return: dictionary of unpickled data
-	"""
-	with open(file, 'rb') as fo:
-		dict = pickle.load(fo, encoding='bytes')
-	return dict
+	# Read in numbers from labels.
+	for subdir, dirs, files in os.walk(labels_path):
+		for file in files:
+			file_path = os.path.join(subdir, file)
+			key = subdir.replace(labels_path, '')[1:] # remove first / to have same key as images
+			with open(file_path, 'r') as f:
+				# Remove new line character at end and cast as float
+				label = float(f.read().rstrip('\n'))
+				if key in data_dictionary:
+					data_dictionary[key].append(label)
 
+	data_dictionary_clean = {}
 
-def get_data(file_path, first_class, second_class):
-	"""
-	Given a file path and two target classes, returns an array of
-	normalized inputs (images) and an array of labels.
-	You will want to first extract only the data that matches the
-	corresponding classes we want (there are 10 classes and we only want 2).
-	You should make sure to normalize all inputs and also turn the labels
-	into one hot vectors using tf.one_hot().
-	Note that because you are using tf.one_hot() for your labels, your
-	labels will be a Tensor, while your inputs will be a NumPy array. This
-	is fine because TensorFlow works with NumPy arrays.
-	:param file_path: file path for inputs and labels, something
-	like 'CIFAR_data_compressed/train'
-	:param first_class:  an integer (0-9) representing the first target
-	class in the CIFAR10 dataset, for a cat, this would be a 3
-	:param first_class:  an integer (0-9) representing the second target
-	class in the CIFAR10 dataset, for a dog, this would be a 5
-	:return: normalized NumPy array of inputs and tensor of labels, where
-	inputs are of type np.float32 and has size (num_inputs, width, height, num_channels) and labels
-	has size (num_examples, num_classes)
-	"""
-	unpickled_file = unpickle(file_path)
-	inputs = unpickled_file[b'data']
-	labels = unpickled_file[b'labels']
-	# TODO: Do the rest of preprocessing!
-	inputs = 1/255.0 * inputs.reshape(len(inputs), 3, 32, 32).transpose(0, 2, 3, 1)
+	# Remove images and labels with no label.
+	for key in data_dictionary:
+		if key != '' and len(data_dictionary[key]) == 2:
+			data_dictionary_clean[key] = data_dictionary[key]
 
-	bad_indices = []
-	on_values = []
-	for i in range(len(labels)):
-		if labels[i] == first_class:
-			labels[i] = 0
-		elif labels[i] == second_class:
-			labels[i] = 1
-		else:
-			bad_indices.append(i)
+	return data_dictionary_clean
 
-
-
-	inputs = np.delete(inputs, bad_indices, axis = 0)
-	labels = np.delete(labels, bad_indices)
-
-	labels = tf.one_hot(labels, depth = 2)
-
-	return np.float32(inputs), labels
+print(get_data('/home/awei6/course/cohn-kanade-images/', '/home/awei6/course/Emotion'))
